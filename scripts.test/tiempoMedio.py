bc57d2d580e53b36dfa67906eeef91ed311efd03
DaGal <galaktionov@gmail.com>
2018-11-09 14:23:19 +0100

    Proper avgTime

--- scripts.test/tiempoMedio.py
+++ scripts.test/tiempoMedio.py
@@ -13,11 +13,29 @@ def encode_time(t):
 	return "%02d:%02d:%02d" % (t/3600, (t/60) % 60, t % 60)
 
 def main(argv):
-	counter = collections.Counter()
+	if len(argv) < 2:
+		print("Usage: tiempoMedio.py <lineStops.txt>")
+		sys.exit(1)
+
+	line_stops = {}
 	line_times = {}
+	line_freqs = {}
 	current_line = None
 
+	with open(argv[1]) as lineStops:
+		for line in lineStops:
+			line_match = re.match("([^:]+):\s*([^\s]+)$", line)
+
+			if line_match:
+				current_line = line_match.group(1)
+				stops = line_match.group(2).split(",")
+				line_stops[current_line] = stops
+				line_times[current_line] = [0] * len(stops)
+				line_freqs[current_line] = [0] * len(stops)
+
+
 	for line in sys.stdin:
+		line = line.strip()
 		day_match = re.match("DAY (\d+)", line)
 		line_match = re.match("([^:]+):\s*$", line)
 		time_match = re.match("^([0-9:]+)-", line)
@@ -26,27 +44,22 @@ def main(argv):
 			pass
 		elif line_match:
 			current_line = line_match.group(1)
-			counter.update([current_line])
 		elif time_match:
-			times = [parse_time(s.split("-")[0]) for s in line.split(",")]
-			times = [t-times[0] for t in times[1:]]
+			t0 = parse_time(time_match[0][:-1])
+			stops = [(parse_time(t[0])-t0, t[1]) for t in [s.split("-") for s in line.split(",")]]
+			last_i = 0
 
-			if current_line in line_times:
-				if len(line_times[current_line]) != len(times):
-					if len(line_times[current_line]) < len(times):
-						line_times[current_line] = times
-						counter[current_line] = 1
-					else:
-						continue
-
-				line_times[current_line] = [a+b for a,b in zip(line_times[current_line], times)]
-				counter.update([current_line])
-			else:
-				line_times[current_line] = times
-				counter[current_line] = 1
+			for s in stops:
+				try:
+					i = line_stops[current_line].index(s[1], last_i)
+					line_times[current_line][i] += s[0]
+					line_freqs[current_line][i] += 1
+					last_i = i
+				except ValueError:
+					pass
 
 	for line,times in line_times.items():
-		print("%s: %s" % (line, ",".join([str(int(t/counter[line])) for t in times])))
+		print("%s: %s" % (line, ",".join([str(int(t/max(f,1))) for t,f in zip(times, line_freqs[line])])))
 
 
 if __name__ == "__main__":
