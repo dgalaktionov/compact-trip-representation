38a6cf35ef7a7be3870ef690c7677deb9c496285
dagal <galaktionov@gmail.com>
2019-02-05 13:06:07 +0100

    Remove sudo from docker

--- scripts.test/goDocker.sh
+++ scripts.test/goDocker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ..
-sudo docker build -t trippy --build-arg USER=$USER --build-arg UID=$UID . &&
-sudo docker run -it --name=trippy_exp -v $(pwd):/data trippy
-sudo docker rm trippy_exp
+docker build -t trippy --build-arg USER=$USER --build-arg UID=$UID . &&
+docker run -it --name=trippy_exp -v $(pwd):/data trippy
+docker rm trippy_exp
 cd scripts.test
