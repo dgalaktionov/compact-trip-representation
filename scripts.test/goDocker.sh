8a269ff017648a61ba9d80176149b6126a0f16bb
dagal <dagal@vivaldi.net>
2019-02-07 15:18:26 +0100

    Fix goDocker.sh

--- scripts.test/goDocker.sh
+++ scripts.test/goDocker.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 cd ..
+docker stop trippy_exp
+docker rm trippy_exp
 docker build -t trippy --build-arg USER=$USER --build-arg UID=$UID . &&
 docker run -it --name=trippy_exp -v $(pwd):/data trippy
-docker rm trippy_exp
+docker stop trippy_exp
 cd scripts.test
